ae5d8f4d6aa339a934301bad95113fb9c9e84a41
prajwal-nepal <161552530+prajwal-nepal@users.noreply.github.com>
2024-07-18 17:02:32 +0545

    Services

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using CRUD_OPERATION_WEB_APP.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_OPERATION_WEB_APP.Services
+{
+    public class ApplicationDbContext(DbContextOptions options) : DbContext(options)
+
+    {
+		public DbSet<Product> Products { get; set; }
+    }
+}
